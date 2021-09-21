97c246b08dada605db678248e9cb4b74cca07124
Chioma Joshua <chiomajoshua@gmail.com>
2021-09-21 10:06:41 +0100

    Code Refactoring

--- Core/Enums.cs
+++ Core/Enums.cs
@@ -6,21 +6,13 @@ namespace AllergiesSolution.Core
     {
         public enum Allergen
         {
-            [EnumMember(Value = "eggs")]
             Eggs = 1,
-            [EnumMember(Value = "peanuts")]
             Peanuts = 2,
-            [EnumMember(Value = "shellfish")]
             Shellfish = 4,
-            [EnumMember(Value = "strawberries")]
             Strawberries = 8,
-            [EnumMember(Value = "tomatoes")]
             Tomatoes = 16,
-            [EnumMember(Value = "chocolate")]
             Chocolate = 32,
-            [EnumMember(Value = "pollen")]
             Pollen = 64,
-            [EnumMember(Value = "cats")]
             Cats = 128
         }
     }
