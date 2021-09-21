dcb528dfb315114ed97659bc4e45bb464d3183bb
Chioma Joshua <chiomajoshua@gmail.com>
2021-09-21 09:58:13 +0100

    Writing Test Cases in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using AllergiesSolution.Core;
+using AllergiesSolution.Core;
 using System;
+using static AllergiesSolution.Core.Enums;
 
 namespace AllergiesSolution
 {
@@ -7,13 +8,31 @@ namespace AllergiesSolution
     {
         public static void Main(string[] args)
         {
+            var allergyService = new Allergies();
+            
+            //AddAllergy(string input)
+            allergyService.AddAllergy("Tomatoes");
+            Console.WriteLine($"Allergy Score is : { allergyService.Score}");
+            
+            //AddAllergy(enum input)
+            allergyService.AddAllergy(Allergen.Cats);
+            Console.WriteLine($"Allergy Score is : { allergyService.Score}");
+            
+            //DeleteAllergy(enum input)
+            allergyService.DeleteAllergy(Allergen.Shellfish);
+            Console.WriteLine($"Allergy Score is : { allergyService.Score}");
+                
+            //DeleteAllergy(string input)
+            allergyService.DeleteAllergy("Peanuts");
+            Console.WriteLine($"Allergy Score is : { allergyService.Score}");
+
             var allergy = new Allergies("Mark");
             Console.WriteLine(allergy.ToString());
 
-            var scoreAllergy = new Allergies("Dorcas", 82);            
+            var scoreAllergy = new Allergies("Dorcas", allergyService.Score);            
             Console.WriteLine(scoreAllergy.ToString());
 
-            var descriptionAllergy = new Allergies("Dorcas", "Peanuts Chocolate Cats Strawberries");
+            var descriptionAllergy = new Allergies("Richie", "Peanuts Chocolate Cats Strawberries");
             Console.WriteLine(descriptionAllergy.ToString());
 
             Console.ReadKey();
